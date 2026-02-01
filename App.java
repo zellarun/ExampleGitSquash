849b19d96a9b558290fd49bb2a4fbea69ed33309
zellarun <zellarun@gmail.com>
2026-02-01 15:37:48 -0700

    Revision 5 in branch (Completed #123)

--- App.java
+++ App.java
@@ -1,11 +1,10 @@
 public class App
 {
-    //this is the chronological revision 3 in branch
+    //this is the chronological revision 5 in branch
     
     public void sampleMethod()
     {
         System.out.println("We'll assume that this change implements task #117");
-        System.out.println("Work in progress...");
         System.out.println("Starting the implementation of task #123");
         //added new unit tests
         //fixed bugs discovered during pair programming
@@ -13,10 +12,9 @@ public class App
         //refactored code
         //fixed code formatting issues
         //etc.
-        System.out.println("More progress towards implementing task #123");
-        /*added more unstable code...*/
-                /*added code that does not follow the team style guides...*/
-            /*added System.out.println debug statements...*/
-        /*the text in these block comments will have to be removed in future commits*/
+        System.out.println("More progress towards implementing task #123"); 
+        //updated comments / documentation
+        //final touches and cleanup
+        System.out.println("Completed the implementation of task #123"); 
     }
 }
