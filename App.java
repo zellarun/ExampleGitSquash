a19f5cbd796c79c18444a85fa743bec44bcf2e36
zellarun <zellarun@gmail.com>
2026-02-01 15:36:11 -0700

    Revision 2 in branch ([WIP] Start the implementation of task #123)

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App
 {
-    //this is the chronological revision 1 in master
+    //this is the chronological revision 2 in branch
     
     public void sampleMethod()
     {
         System.out.println("We'll assume that this change implements task #117");
+        System.out.println("Work in progress...");
+        System.out.println("Starting the implementation of task #123");
     }
 }
